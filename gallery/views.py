57946fca4b6462bcfef3ee46c98cc2385750a8de
Augusto <augustovmagro@gmail.com>
2024-05-14 00:22:34 -0300

    Adding index and Image pages

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def imagem(request):
+    return render(request, 'imagem.html')
+# Create your vews here.
